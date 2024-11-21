d077911577c0b3a82216e033e2dab9b8be9ccc8a
GBalnt20 <balintgheczy@gmail.com>
2024-11-21 14:44:27 +0100

    kész

--- src/jatek/KoPapirOllo.java
+++ src/jatek/KoPapirOllo.java
@@ -12,22 +12,39 @@ public class KoPapirOllo {
         
         Scanner sc = new Scanner(System.in);
         /*Kérdés amire várjuk a választ.*/
-        System.out.println("Szöveg: ");
+//        System.out.println("Szöveg: ");
         /*Eltárpljuk megfelelő típusban a választ*/
-        int egesz = sc.nextInt(); /*Szám bekérése*/
+//        int egesz = sc.nextInt(); /*Szám bekérése*/
         /*Bekérés eredményének kiírása*/
-        System.out.println("Bekért szöveg: " + egesz*2);/*Bekért szám 2x-nek kiírása*/
+//        System.out.println("Bekért szöveg: " + egesz*2);/*Bekért szám 2x-nek kiírása*/
         
         /*Csak akkor, ha szám után szöveget kérünk be, mert a szám utáni ENTER
         (\r\n) "ott marad" ezt olvasa ki*/
-        
         sc.nextLine();
         
-        
-        System.out.println("Szöveg: ");
+//        System.out.println("Szöveg: ");
         /*Szöveg bekérés*/
-        String szoveg = sc.nextLine();
-        System.out.println("Bekért szöveg: "); /*Bekért szöveg kiírása*/
+//        String szoveg = sc.nextLine();
+//        System.out.println("Bekért szöveg: "); /*Bekért szöveg kiírása*/
+        
+        
+        
+        System.out.println("Üdvözöllek a Kő Papír Olló játékban!");
+        System.out.println("Kérlek írj ide valamit: ");
+        String [] lehetosegek = {"Kő", "Papír", "Olló"};
+        Random ran = new Random();
+        String szamitogep = lehetosegek[ran.nextInt(3)];
+        String jatekos = sc.nextLine();
+        if (jatekos.equals(szamitogep)) {
+            System.out.println("Döntetlen");
+        } else if ((jatekos.equals("Kő") && szamitogep.equals("Olló")) ||
+                  (jatekos.equals("Olló") && szamitogep.equals("Papír")) ||
+                  (jatekos.equals("Papír") && szamitogep.equals("Kő"))) {
+            System.out.println("Gratulálok, Te Nyerél!");
+        } else {
+            System.out.println("Sajnlunk, Vesztettél...");
+        }
+        
         
         
         
