a2eb0ecb5d469e245820ec218901a6353686806d
GBalnt20 <balintgheczy@gmail.com>
2024-11-21 15:51:37 +0100

    TeljesenKész

--- src/jatek/KoPapirOllo.java
+++ src/jatek/KoPapirOllo.java
@@ -3,13 +3,10 @@ package jatek;
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class KoPapirOllo {
-    
-    
-     
+
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         /*Kérdés amire várjuk a választ.*/
 //        System.out.println("Szöveg: ");
@@ -17,37 +14,45 @@ public class KoPapirOllo {
 //        int egesz = sc.nextInt(); /*Szám bekérése*/
         /*Bekérés eredményének kiírása*/
 //        System.out.println("Bekért szöveg: " + egesz*2);/*Bekért szám 2x-nek kiírása*/
-        
+
         /*Csak akkor, ha szám után szöveget kérünk be, mert a szám utáni ENTER
         (\r\n) "ott marad" ezt olvasa ki*/
-        sc.nextLine();
-        
+//        sc.nextLine();
 //        System.out.println("Szöveg: ");
         /*Szöveg bekérés*/
 //        String szoveg = sc.nextLine();
 //        System.out.println("Bekért szöveg: "); /*Bekért szöveg kiírása*/
-        
-        
-        
+
         System.out.println("Üdvözöllek a Kő Papír Olló játékban!");
-        System.out.println("Kérlek írj ide valamit: ");
-        String [] lehetosegek = {"Kő", "Papír", "Olló"};
-        Random ran = new Random();
-        String szamitogep = lehetosegek[ran.nextInt(3)];
-        String jatekos = sc.nextLine();
-        if (jatekos.equals(szamitogep)) {
-            System.out.println("Döntetlen");
-        } else if ((jatekos.equals("Kő") && szamitogep.equals("Olló")) ||
-                  (jatekos.equals("Olló") && szamitogep.equals("Papír")) ||
-                  (jatekos.equals("Papír") && szamitogep.equals("Kő"))) {
-            System.out.println("Gratulálok, Te Nyerél!");
-        } else {
-            System.out.println("Sajnlunk, Vesztettél...");
+        System.out.println("Kérlek írj ide valamit a következők közűl: Kő-Papí-Olló");
+        String[] lehetosegek = {"Kő", "Papír", "Olló"};
+
+        while (true) {
+            String jatekos = sc.nextLine();
+            if (!jatekos.equals("Kő")
+                    && !jatekos.equals("Olló")
+                    && !jatekos.equals("Papír")) {
+                System.out.println("Érvénytelen választás, Kérlek válassz a megadott szavak közül!");
+                continue;
+            }
+
+            Random ran = new Random();
+            String szamitogep = lehetosegek[ran.nextInt(3)];
+
+            System.out.println("Játékos választása: " + jatekos);
+            System.out.println("Számítógép választása: " + szamitogep);
+
+            if (jatekos.equals(szamitogep)) {
+                System.out.println("Döntetlen");
+            } else if ((jatekos.equals("Kő") && szamitogep.equals("Olló"))
+                    || (jatekos.equals("Olló") && szamitogep.equals("Papír"))
+                    || (jatekos.equals("Papír") && szamitogep.equals("Kő"))) {
+                System.out.println("Gratulálok, Te Nyerél!");
+            } else {
+                System.out.println("Sajnáljuk, Vesztettél...");
+            }
         }
-        
-        
-        
-        
+
     }
-    
+
 }
